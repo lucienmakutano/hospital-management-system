fb8c9eac08665f27581a60dc95de32bb674248b5
lucien makutano <irlucien.makutano@gmail.com>
2019-09-30 09:56:07 +0300

    adding a layout template

---
+++ st_marys_hospital/application/views/layouts/layout.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <?php start_blockmarker('title') ?>
+        <title></title>
+        <?php end_blockmarker() ?>
+        <?php start_blockmarker('extra_head') ?>
+        <?php end_blockmarker() ?>
+    </head>
+    <body>
+        <?php start_blockmarker('body') ?>
+        <?php end_blockmarker() ?>
+    </body>
+</html>
